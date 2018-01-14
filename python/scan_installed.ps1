a257720c1e4fd898fb79a4aa8765e6bed9d95ad7
Jean <Jean13@users.noreply.github.com>
2018-01-13 22:09:47 -0500

    Update scan_installed.ps1

--- python/scan_installed.ps1
+++ python/scan_installed.ps1
@@ -6,9 +6,9 @@ $arch = Get-WmiObject -class win32_operatingsystem | select -expand OSArchitectu
 if ($arch -eq "64-bit")
 {
   # Get installed packages information (Windows 64-bit)
-  $a = Get-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*
-  $b = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*
-  $c = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*
+  $a = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*
+  $b = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*
+  $c = Get-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*
   $registries = $a + $b + $c
 }
 
@@ -40,12 +40,27 @@ Remove-Item $filename
 $ver = Get-WmiObject -class win32_operatingsystem | select Caption,ServicePackMajorVersion,OSArchitecture
 #Add-Content -Path $newFileName -Value $h
 #Add-Content -Path $newFileName -Value $ver
-$ver_file = "windows_version.txt"
-$ver >> $ver_file
+$dev_file = "device_data.txt"
+$ver >> $dev_file
 
+# Get the device details
+$mod_man = wmic computersystem get model","manufacturer
+$device_ver = wmic csproduct get vendor","version
+$mod_man >> $dev_file
+$device_ver >> $dev_file
+
+# Operating System. Temp file.
+$os_file = "windows_ver.txt"
+$os = Get-WmiObject -class win32_operatingsystem | select Caption
+$os >> $os_file
+
+# Vendor and version information for the database. Temp file.
+$vv_file = "vendor_version.txt"
+$vv = $device_ver | select-object -skip 2
+$vv | Out-File $vv_file -encoding Utf8
 
 # Get list of installed KB's
 wmic qfe get HotFixID","InstalledOn
-$get_kb = wmic qfe get HotFixID
+$get_kb = wmic qfe get HotFixID | findstr /r /v "^$"
 $kb_file = "kb_list.txt"
-$get_kb >> $kb_file
+$get_kb | Out-File $kb_file -encoding Utf8
